4d5a23531ae3d012e4d8eca07cabcfff624722cc
Mustafa DEMİR <mustafa.demir@bil.omu.edu.tr>
2016-12-10 19:14:32 +0200

    Add files via upload

---
+++ Mainwindow.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;// mail işlemleri için
+using System.Net;
+using System.IO; //dosya işlemleri için
+using System.Security.Cryptography;
+using System.Data.SqlClient;
+namespace mustafa
+{
+    public partial class Mainwindow : Form
+    {
+        
+        
+        
+        public Mainwindow()
+        {
+            InitializeComponent();
+            
+            
+        }
+        SqlConnection baglanti = new SqlConnection(@"server=.\SQLEXPRESS ; database=odev3 ; Trusted_connection=yes");
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            paketle();
+            String dosyayolu = @"C:\Users\mustafademir\Desktop\KKKLASOR\Belge.txt";
+            try
+            {
+                SmtpClient sc = new SmtpClient();
+                sc.Port = 587;
+                sc.Host = "smtp.gmail.com";
+                sc.EnableSsl = true;
+                sc.Credentials = new NetworkCredential("mustafa.demir@bil.omu.edu.tr", "12060385");
+
+
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("mustafa.demir@bil.omu.edu.tr");
+                mail.IsBodyHtml = false;
+                mail.To.Add(comboBox1.Text.ToString());
+                mail.Subject = richTextBox1.Text.ToString();
+                mail.Body = "Bu Mail size USECE-Mail uygulamasıyla gönderilmiştir.."+
+                    "Mesaj içeriğini görmek için uygulamamızı indirmelisiniz";
+                mail.Attachments.Add(new Attachment(dosyayolu));
+                sc.Send(mail);
+                MessageBox.Show("Mail Başarıyla GÖnderildi.", "Mail Gönderme");
+            }
+            catch
+            {
+                MessageBox.Show("Mail Gönderme İşlemi Başarısız.", "Mail Gönderme");
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+        private void kisigoster()
+        {
+            baglanti.Open();
+            SqlCommand kmt = new SqlCommand("Select * from tOgrenci", baglanti);
+
+            SqlDataReader rdr = kmt.ExecuteReader();
+            while (rdr.Read())
+            {
+                ListViewItem li = new ListViewItem();
+                li.Text = rdr["OgrenciID"].ToString();
+                li.SubItems.Add(rdr["ad"].ToString());
+                listView1.Items.Add(li);
+            }
+            baglanti.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            kisigoster();
+        }
+        private void paketle()
+        {
+            
+            StreamWriter sw = new StreamWriter(@"C:\Users\mustafademir\Desktop\KKKLASOR\Belge.txt");
+            sw.WriteLine(cry.SifreleAES(richTextBox2.Text, "12345678901234567890123456789012"));
+            sw.Close();
+            
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dos = new OpenFileDialog();
+            dos.ShowDialog();
+            if (dos.FileName != null)
+            {
+                string yol = dos.FileName;
+                StreamReader oku = new StreamReader(yol);
+
+                oku = File.OpenText(yol);
+                yol = null;
+                string metin = oku.ReadToEnd();
+
+                oku.Close();
+                string cozmetin = cry.SifreyiCozAES(metin, "12345678901234567890123456789012");
+                richTextBox3.Text = cozmetin;
+            }
+            else
+            {
+                MessageBox.Show("dosya secmediniz");
+            }
+        }
+    }
+}
---
+++ cry.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace mustafa
+{
+    public class cry
+    {
+        private static byte[] _salt = Encoding.ASCII.GetBytes("o6806642kbM7c5");
+        public static string GenerateNewPassword(int size)
+        {
+            char[] cr = "0123456789abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            string result = string.Empty;
+            Random r = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                result += cr[r.Next(0, cr.Length - 1)].ToString();
+            }
+            return result;
+        }
+        
+        public static string SifreleAES(string plainText, string sharedSecret)
+        {
+            
+            if (string.IsNullOrEmpty(plainText))
+                throw new ArgumentNullException("plainText");
+            if (string.IsNullOrEmpty(sharedSecret))
+                throw new ArgumentNullException("sharedSecret");
+
+            string outStr = null;
+            RijndaelManaged aesAlg = null;
+
+            try
+            {
+
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(sharedSecret, _salt);
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+
+                    msEncrypt.Write(BitConverter.GetBytes(aesAlg.IV.Length), 0, sizeof(int));
+                    msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    outStr = Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+            finally
+            {
+
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+
+            return outStr;
+        }
+        /// <summary>
+        /// EncryptStringAES() ile şifrelenen metnin şifresini çözer.        
+        /// <param name="cipherText">Deşifre edilecek metin</param>
+        /// <param name="sharedSecret">Paylaşılan gizli anahtar</param>
+        public static string SifreyiCozAES(string cipherText, string sharedSecret)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+                throw new ArgumentNullException("cipherText");
+            if (string.IsNullOrEmpty(sharedSecret))
+                throw new ArgumentNullException("sharedSecret");
+
+
+            RijndaelManaged aesAlg = null;
+
+
+            string plaintext = null;
+
+            try
+            {
+
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(sharedSecret, _salt);
+
+
+                byte[] bytes = Convert.FromBase64String(cipherText);
+                using (MemoryStream msDecrypt = new MemoryStream(bytes))
+                {
+
+                    aesAlg = new RijndaelManaged();
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                    aesAlg.IV = ReadByteArray(msDecrypt);
+
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+
+
+                            plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+            finally
+            {
+
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+            return plaintext;
+        }
+
+        private static byte[] ReadByteArray(Stream s)
+        {
+            byte[] rawLength = new byte[sizeof(int)];
+            if (s.Read(rawLength, 0, rawLength.Length) != rawLength.Length)
+            {
+                throw new SystemException("Stream did not contain properly formatted byte array");
+            }
+
+            byte[] buffer = new byte[BitConverter.ToInt32(rawLength, 0)];
+            if (s.Read(buffer, 0, buffer.Length) != buffer.Length)
+            {
+                throw new SystemException("Did not read byte array properly");
+            }
+
+            return buffer;
+        }
+    }
+}
